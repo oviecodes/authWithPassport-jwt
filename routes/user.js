9045dc14371eda82d31609956fa6ea96851b8843
Godwin Alexander <alecgee73@gmail.com>
2020-10-24 05:00:01 +0100

    create user route

---
+++ routes/user.js
@@ -0,0 +1,28 @@
+
+
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+const { getSignUp, createUser } = require('../controllers/signUpController')
+const { getLogin, authUser } = require('../controllers/loginController')
+
+router.route('/signup')
+    .get(getSignUp)
+    .post(createUser)
+
+
+router.route('/login')
+    .get(getLogin)
+    .post(authUser)
+
+
+router.route('/dashboard')
+    .all(passport.authenticate('jwt', { session: false }))
+    .get(async(req, res) => {
+        
+        console.log(req.user)
+        res.status(200).json({ msg: `welcome to your dashboard` })
+    })
+    .post()
+
+module.exports = router
